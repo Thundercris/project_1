3232f81d95c9d33f0eaa31e0b5c9fd7e183b8b5b
Cristina Grande <cri.grande@gmail.com>
2021-06-02 17:58:39 +0200

    after Sam chat + previous code commented

--- scripts/app.js
+++ scripts/app.js
@@ -16,6 +16,7 @@ function init() {
   function createGrid(discStartPosition) {
     for (let i = 0; i < cellCount; i++) { 
       const cell = document.createElement('div') 
+      cell.id = i
       cell.classList.add('cell')
       cell.innerText = i 
       grid.appendChild(cell) 
@@ -33,9 +34,9 @@ function init() {
   const playerChoice = { currentChoice: null }
   const computerChoice = { currentChoice: null }
   const choosenElement = []
-  
-  
-  let arrayChoice 
+  let randomizeValue = []
+
+  let currentPlayer = 'player'
 
   let choices = [[0,7,14,21,28,35],[1,8,15,22,29,36],[2,9,16,23,30,37],[3,10,17,24,31,38],[4,11,18,25,32,39],[5,12,19,26,33,40],[6,13,20,27,34,41]]
 
@@ -50,27 +51,70 @@ function init() {
   function insertDisc(cell) {
     // console.log(cell)
     // convert the div class into a number
-    const convertedNumber = parseInt(cell.textContent) 
-    // console.log(convertedNumber)
+    const convertedNumber = parseInt(cell.id) 
+    console.log(cell.id)
+    console.log(convertedNumber)
     //check in which array the "convertedNumber" belongs using the return array function, then give me back the array
     const arrayResult = returnArray(convertedNumber) 
-    // console.log(arrayResult)
-    //check in the array which is the biggest element and in this way I will choose always the lowest cell in the column
-    const maxValue = Math.max.apply(null, arrayResult)
-    // console.log(maxValue)
-    const indexArray  = getTheArrayIndex(maxValue)
-    console.log(indexArray)
+    console.log(arrayResult)
+
+    const availableSpaces = arrayResult.filter(item => !cells[item].classList.contains('disc'))
+    console.log(availableSpaces)
+    cells[Math.max.apply(null, availableSpaces)].classList.add('disc')
+    cells[Math.max.apply(null, availableSpaces)].classList.add(currentPlayer)
+    console.log(cells[Math.max.apply(null, availableSpaces)])
+    //swap player
+  
+    swapPlayer()  
+  }
+
+  function swapPlayer() {
+    if (currentPlayer === 'player') {
+      currentPlayer = 'computer'
+      computerTurn()
+    } else {
+      currentPlayer = 'player'
+    } 
+  }
+  function computerTurn() {
+    const randomNumber = Math.floor(Math.random() * 7)
+    insertDisc(cells[randomNumber])
+  }
+
+  function name(params) {
     
   }
-  // I need to get the position of maxValue once I find it remove it from the array and + into choosenElement
-  function getTheArrayIndex(maxValue) {
-    for (let i = 0; i < choices.length; i++) {
-      if (choices[i].indexOf(maxValue) !== -1){
-        return choices[i].indexOf(maxValue)
-      }
-    }
-  } 
+
+  // console.log(arrayResult)
+  //check in the array which is the biggest element and in this way I will choose always the lowest cell in the column
+  // const maxValue = Math.max.apply(null, arrayResult)
+  // console.log(maxValue)
+  // //insert disc class
+  // cells[maxValue].classList.add('player')
+  // //this is the position of the maxValue in the array
+  // const indexArray  = getTheArrayIndex(maxValue)
+  // console.log(indexArray)
+  // //Position of the arrayResult that contain the maxValue
+  // const arrayPosition = choices.indexOf(arrayResult)
+  // console.log(arrayPosition)
+  // //Remove maxValue from choices
+  // choices[arrayPosition] = choices[arrayPosition].slice(0, indexArray) //I have the issue of -1 so I might need to put all in a function
+  // //Push maxValue into choosenElement (only one array for now to test it)
+  // choosenElement.push(maxValue)
+  // console.log(choices)
+  // console.log(choosenElement)
+
   
+  
+  // // I need to get the position of maxValue once I find it remove it from the array and + into choosenElement
+  // function getTheArrayIndex(maxValue) {
+  //   for (let i = 0; i < choices.length; i++) {
+  //     if (choices[i].indexOf(maxValue) !== -1){
+  //       return choices[i].indexOf(maxValue)
+  //     }
+  //   }
+  // } 
+
   // I need to create the possible choices, the idea is having the arrays of each column, in this way I can also create a rule in order to choose always the n-1. 
   // I need then to define the choice of both, human and computer(random)
   // make the event listener working for all button that I click
@@ -99,42 +143,42 @@ function init() {
   // }
   // playerChoose()
 
-  function computerChoose() {
-    //computer choose random value from choices, in order to randomize what I can do is to convert choices in one array cointaining all element instead of having them split into arrays.
-    //but how can I pass the problem of the biggest value? maybe I can  get the last element (the biggest) of each array and then randomize only one element of each array? X.X
-  }
+  // function computerChoose() {
+  //   //computer choose random value from choices, in order to randomize what I can do is to convert choices in one array cointaining all element instead of having them split into arrays.
+  //   //but how can I pass the problem of the biggest value? maybe I can  get the last element (the biggest) of each array and then randomize only one element of each array? X.X
+    
+  // }
 
-  
-  class Game {
-    constructor (players, totalDiscs) {
-      this.players = players
-      this.totalDiscs = totalDiscs
-    } 
-    playerMove(player) {
-      // if (player.type === 'Human' || player.type === 'Computer' && this.discQuantity >= 21) {
-      //   player.total++
-      //   this.totalDiscs--
-      // } else 
-      //   alert('TIE!')
-    } 
-  }
+  // class Game {
+  //   constructor (players, totalDiscs) {
+  //     this.players = players
+  //     this.totalDiscs = totalDiscs
+  //   } 
+  //   playerMove(player) {
+  //     // if (player.type === 'Human' || player.type === 'Computer' && this.discQuantity >= 21) {
+  //     //   player.total++
+  //     //   this.totalDiscs--
+  //     // } else 
+  //     //   alert('TIE!')
+  //   } 
+  // }
 
-  class Player {
-    constructor (type, discColor, discQuantity) {
-      this.type = type
-      this.discColor = discColor
-      this.discQuantity = discQuantity
-      this.total = 0
-    }
-  }
-  const playerHuman =  new Player('Human', 'red', 21)
-  const playerComputer = new Player('Computer', 'yellow', 21)
-  const connectGame = new Game([playerHuman, playerComputer], 42)
-  console.log(playerHuman)
-  console.log(playerComputer)
-  console.log(connectGame)
+  // class Player {
+  //   constructor (type, discColor, discQuantity) {
+  //     this.type = type
+  //     this.discColor = discColor
+  //     this.discQuantity = discQuantity
+  //     this.total = 0
+  //   }
+  // }
+  // const playerHuman =  new Player('Human', 'red', 21)
+  // const playerComputer = new Player('Computer', 'yellow', 21)
+  // const connectGame = new Game([playerHuman, playerComputer], 42)
+  // console.log(playerHuman)
+  // console.log(playerComputer)
+  // console.log(connectGame)
 
-  cellSelect.forEach(cell => {
+  cells.forEach(cell => {
     // cell.addEventListener('click', insertDisc)
     cell.addEventListener('click', function(){
       insertDisc(cell)  
